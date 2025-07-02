7238b802e1724fecfd7d7e251aa83dde4bcb9a3e
gallanpw <gallan.widyanto@gmail.com>
2025-07-02 08:59:42 +0700

    Tugas 2 - Layouting & Styling (Pages Router)

---
+++ tugas-nextjs-pages-router/components/layout.tsx
@@ -0,0 +1,18 @@
+import Navbar from './navbar';
+import Footer from './footer';
+import React from 'react';
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    // Kontainer utama untuk layout, membuatnya menjadi flex column
+    // dan memastikan tinggi minimalnya adalah 100% dari viewport
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      {/* Konten utama yang akan mengisi sisa ruang yang tersedia */}
+      <main className="flex-grow">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+}
