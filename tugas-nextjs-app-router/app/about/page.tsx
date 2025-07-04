b553c27f2d604a93a257942e8d4f41f89e7607fe
gallanpw <gallan.widyanto@gmail.com>
2025-07-04 10:25:13 +0700

    fixing: Tugas 4 - Layouting & Styling (App Router)

---
+++ tugas-nextjs-app-router/app/about/page.tsx
@@ -0,0 +1,11 @@
+export default function About() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <h1 className="text-3xl font-bold">About Page</h1>
+        <p className="text-lg">
+            This is the about page of our Next.js application. Here you can find
+            information about the app and its features.
+        </p>
+        </div>
+    );
+}
---
+++ tugas-nextjs-app-router/app/blog/page.tsx
@@ -0,0 +1,11 @@
+export default function Blog() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <h1 className="text-3xl font-bold">Blog Page</h1>
+            <p className="text-lg">
+                Welcome to the blog page of our Next.js application. Here you can find
+                various articles and posts related to our topics of interest.
+            </p>
+        </div>
+    );
+}
