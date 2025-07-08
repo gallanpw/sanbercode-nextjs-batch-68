9af8df97f99ff26de0e2200416dec076c560f92e
gallanpw <gallan.widyanto@gmail.com>
2025-07-08 09:18:49 +0700

    feature: add req method post Tugas 6 - API Routes (Pages Router)

--- tugas-nextjs-pages-router/pages/api/user/[id].tsx
+++ tugas-nextjs-pages-router/pages/api/user/[id].tsx
@@ -1,15 +1,32 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { forbidden } from 'next/navigation';
 
 type Data = {
-  id: string | string[] | undefined;
-  name: string;
-  message: string;
+    id?: string | string[] | undefined
+    name?: string
+    message?: string
+    data?: object
+    headers?: string | string[] | undefined
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({
-    id: req?.query?.id,
-    name: 'John Doe',
-    message: 'Hello, Next.js API!'
-  } as Data);
+    if (req.method === 'GET') {
+            res.status(200).json({
+            id: req?.query?.id,
+            name: 'John Doe',
+            message: 'Hello, Next.js API!'
+        } as Data);
+    } 
+    
+    if (req.method === 'POST') {
+        res.status(200).json({
+            id: req?.query?.id,
+            name: 'John Doe',
+            message: 'Hello, Next.js API!',
+            data: req.body,
+            headers: req.headers['api-token']
+        })
+    } else {
+        res.status(403).json({ message: 'forbidden' })
+    }
 }
